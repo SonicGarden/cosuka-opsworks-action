fa6673fcf843e06605b3f183b777fe926ab19492
aki <lala.akira@gmail.com>
2020-07-10 17:27:13 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,72 @@
+import * as fs from 'fs'
 import * as core from '@actions/core'
-import {command} from 'execa'
+import * as github from '@actions/github'
+import replaceComment from '@aki77/actions-replace-comment'
+import execa, {command} from 'execa'
 
-const getCrontab = async (): Promise<string> => {
-  const {stdout} = await command(
-    'bundle exec rake cosuka_opsworks:output_cron',
+const outputCrontab = async (path: string): Promise<void> => {
+  const subprocess = execa(
+    'bundle',
+    ['exec', 'rake', 'cosuka_opsworks:output_cron'],
     {
       env: {RAILS_ENV: 'test', DISABLE_SPRING: 'true'}
     }
   )
-  return stdout
+  subprocess.stdout?.pipe(fs.createWriteStream(path))
+  await subprocess
 }
+
+const getDiff = async (baseRef: string): Promise<string> => {
+  const HEAD_CRONTAB = 'tmp/head_crontab.txt'
+  const BASE_CRONTAB = 'tmp/base_crontab.txt'
+
+  await outputCrontab(HEAD_CRONTAB)
+
+  await execa('git', ['fetch', 'origin', baseRef])
+  const subProcess = execa('git', [
+    'show',
+    `remotes/origin/${process.env.GITHUB_BASE_REF}:config/schedule.rb`
+  ])
+  subProcess.stdout?.pipe(fs.createWriteStream('config/schedule.rb'))
+  await subProcess
+
+  await outputCrontab(BASE_CRONTAB)
+
+  try {
+    const {stdout} = await command(`diff -u ${HEAD_CRONTAB} ${BASE_CRONTAB}`)
+    return stdout
+  } catch (error) {
+    if (error.stdout) {
+      return error.stdout
+    }
+    throw error
+  }
+}
+
+const CODE = '```'
 
 async function run(): Promise<void> {
   try {
-    const newCrontab = await getCrontab()
-    core.debug(newCrontab)
+    if (!process.env.GITHUB_BASE_REF) {
+      throw new Error('GITHUB_BASE_REF is undefined.')
+    }
+
+    const diff = await getDiff(process.env.GITHUB_BASE_REF)
+
+    const body = `## :warning: Crontab Diff!
+${CODE}diff
+${diff}
+${CODE}
+`
+
+    await replaceComment({
+      token: core.getInput('token', {required: true}),
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      issue_number: github.context.issue.number,
+      body
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
